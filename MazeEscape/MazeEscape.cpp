6a7ff8bc6fb8164bcc9e62f0488670aaec716035
MartinUzunov1707 <muzun1707@gmail.com>
2025-01-15 18:02:03 +0200

    Create identify function.

--- MazeEscape/MazeEscape.cpp
+++ MazeEscape/MazeEscape.cpp
@@ -2,10 +2,68 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+std::fstream currentPlayerProfile;
+
+void usernameValidation(std::string &username) {
+	std::cin >> username;
+	while (username.length() >= 50) {
+		std::cout << "Username must not exceed 50 characters!" << std::endl;
+		std::cin >> username;
+	}
+}
+void loginPlayer(std::string username) {
+
+}
+void registerPlayer(std::string username) {
+	currentPlayerProfile.open(username);
+	if (currentPlayerProfile) {
+		std::cout << "Username already exist. Logged in succesfully!" << std::endl;
+	}
+}
+
+void identify() {
+	std::cout << "Welcome to Dungeon Escape!" << std::endl;
+	std::cout << "1.Login" << std::endl;
+	std::cout << "2.Register" << std::endl;
+	std::cout << "3.Exit" << std::endl;
+	std::cout << "Input: ";
+	char input = 0;
+	std::cin >> input;
+	while (true) {
+		if (input == '1') {
+			std::cout << "Please enter username:";
+			std::string username;
+			usernameValidation(username);
+			username.append(".txt");
+
+			loginPlayer(username);
+			break;
+		}
+		else if (input == '2') {
+			std::cout << "Please enter username:";
+			std::string username;
+			usernameValidation(username);
+			username.append(".txt");
+
+			registerPlayer(username);
+			break;
+		}
+		else if (input == '3') {
+			exit(EXIT_SUCCESS);
+			break;
+		}
+		std::cout << "Invalid command, please try again!" << std::endl;
+		std::cout << "Input: ";
+		std::cin >> input;
+	}
+	
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    identify();
 }
 
 
